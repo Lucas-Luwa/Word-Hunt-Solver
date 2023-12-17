208331b4cfc4cb96eb24f069b307f26a4af3f5aa
Lucas-Luwa <lluwa3@gatech.edu>
2023-12-16 21:26:57 -0500

    UI and Input Validation

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def arrowMovement(event):
         moveEntry(0, -1)
 
 
+
 window = tk.Tk()
-window.geometry("400x400")
+window.geometry("290x350")
 window.configure(bg = 'lightgreen')
 window.title("Word Hunt Solver")
 
-mainlabel = tk.Label(window, text = "Enter Grid ", font=("Helvetica", 16)) 
-# mainlabel.pack()
+mainlabel = tk.Label(window, text = "Enter Grid ", font=("Helvetica", 16), height = 0) 
+# mainlabel.place(x=0, y=2)
 mainlabel.configure(bg = 'lightgreen')
-
 
 entries = []
 for i in range(4):
@@ -45,5 +45,32 @@ window.bind("<Down>", arrowMovement)
 window.bind("<Up>", arrowMovement)
 
 entries[0][0].focus_set()
+
+def submitVals():
+    #Input Validation
+    endTask = False
+    for i in range(4):
+            for j in range(4):
+                if len(entries[i][j].get()) != 1 or not (entries[i][j].get().lower() >= 'a' and entries[i][j].get().lower() <= 'z'):
+                    endTask = True
+                    break
+            if endTask: 
+                print("Invalid Inputs. Please try again.")
+                break
+     
+    if not endTask:
+        data = []
+        for i in range(4):
+            rowData = []
+            for j in range(4):
+                value = entries[i][j].get()
+                rowData.append(value)
+            data.append(rowData)
+
+        for row in data:
+            print(row)
+
+submit_button = tk.Button(window, text="Submit", command=submitVals)
+submit_button.grid(row=5, column=0, columnspan=4, pady=10)
 
 window.mainloop()
