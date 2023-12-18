dd5fa7de3866e1f0318cf73849dad84923ca8b21
Lucas-Luwa <lluwa3@gatech.edu>
2023-12-18 04:16:06 -0500

    Second Part Complete

--- Runner.py
+++ Runner.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def plotter(inputLines, filledTile=None):
+def plotter(inputLines, filledTile=None, initChars=None):
     gridNum = 4
 
     fig = Figure(figsize=(6, 6))
@@ -13,6 +13,13 @@ def plotter(inputLines, filledTile=None):
         start = line[0]; end = line[1]
         plot.plot([start % gridNum, end % gridNum], [start // gridNum, end // gridNum], 'k-')
 
+    counter = 0
+    for x in range(gridNum):
+        for y in range(gridNum):
+            plot.annotate(initChars[counter], (y, x), color='black', fontsize=36,
+                          ha='center', va='center')
+            counter+=1
+    
     if filledTile is not None:
         fillSqX = filledTile % gridNum -0.5
         fillSqY = filledTile // gridNum - 0.5
@@ -30,19 +37,20 @@ def plotter(inputLines, filledTile=None):
 def nxtWord(event):
     global currLineNum
     currLineNum = (currLineNum + 1) % len(allWords)
-    plotter(allWords[currLineNum], initNode[currLineNum])
+    plotter(allWords[currLineNum], initNode[currLineNum], initChars)
     currWord.set(f"Current Word: {wordValues[currLineNum]} | Word #: {currLineNum + 1}/{numElements} | Points: {pointValues[currLineNum]}")
 
 
 def prevWord(event):
     global currLineNum
     currLineNum = (currLineNum - 1) % len(allWords)
-    plotter(allWords[currLineNum], initNode[currLineNum])
+    plotter(allWords[currLineNum], initNode[currLineNum], initChars)
     currWord.set(f"Current Word: {wordValues[currLineNum]} | Word #: {currLineNum + 1}/{numElements} | Points: {pointValues[currLineNum]}")
 
 
-def mainContents(gridContents):
-    global window, currLineNum, allWords, wordValues, currWord, initNode, numElements, pointValues
+def mainContents(gridStart, gridContents):
+    global window, currLineNum, allWords, wordValues, currWord, initNode, numElements, pointValues, initChars
+    initChars = gridStart
     window = tk.Tk()
     window.title("Solved Words")
 
@@ -71,7 +79,7 @@ def mainContents(gridContents):
     wordLabel = tk.Label(window, textvariable=currWord, font=("Arial", 12))
     wordLabel.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
-    plotter(allWords[currLineNum], initNode[0])
+    plotter(allWords[currLineNum], initNode[0], initChars)
 
     window.bind("<Right>", nxtWord)
     window.bind("<Left>", prevWord)
@@ -99,7 +107,7 @@ def pointCalc(num):
     return vals[num - 3]
 
 # Initial Testing
-mainContents([('hedge', '2111102030'), ('badge', '0100102011'), ('badge', '0100102030'), ('bead', '01110010'), ('head', '21110010'), 
+mainContents("abcdefghijklmnop", [('hedge', '2111102030'), ('badge', '0100102011'), ('badge', '0100102030'), ('bead', '01110010'), ('head', '21110010'), 
               ('bade', '01001011'), ('edge', '11102030'), ('ghee', '20213223'), ('abed', '00011110'), ('dae', '100011'), ('edh', '111021'), 
               ('gee', '132332'), ('ged', '201110'), ('beg', '011120'), ('fee', '313223'), ('deg', '101120'), ('fee', '332332'), 
               ('bad', '010010'), ('jed', '221110'), ('fee', '122332'), ('fee', '333223'), ('ade', '001011'), ('fed', '121110'), 
